157397c3060ddfad9a14ebe03d2ce4ca2697191d
pamardiyb <smp.pamardiyb@gmail.com>
2023-09-06 12:59:53 +0700

    fix session bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,17 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { LanguageContextProvider } from '../intl/LanguageProvider';
 import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
 import Layout from '../components/Layout';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <LanguageContextProvider>
         <Layout>
           <Component {...pageProps} />
